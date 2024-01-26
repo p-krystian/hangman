3f5f778e4865b53babaddf630938ee1fea9fddf5
Krystian Piątek <piatekkrystian13@gmail.com>
2024-01-26 10:13:52 +0100

    Remove main element and move Router to index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { Routes, Route} from "react-router-dom"
 import StartMenu from './Screens/Start/Start'
 import SingleMenu from './Screens/Single/Single'
 import LocalMenu from './Screens/Local/Local'
@@ -7,14 +7,12 @@ import MultiMenu from './Screens/Multi/Multi'
 
 function App(){
   return (
-    <Router>
-      <Routes>
-        <Route path='/single' element={ <SingleMenu /> } />
-        <Route path='/local' element={ <LocalMenu /> } />
-        <Route path='/multi' element={ <MultiMenu /> } />
-        <Route path='*' element={ <StartMenu /> } />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path='/single' element={ <SingleMenu /> } />
+      <Route path='/local' element={ <LocalMenu /> } />
+      <Route path='/multi' element={ <MultiMenu /> } />
+      <Route path='*' element={ <StartMenu /> } />
+    </Routes>
   )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App.jsx';
 
@@ -7,8 +8,8 @@ ReactDOM.createRoot(
   document.getElementById('root')
 ).render(
   <React.StrictMode>
-    <main>
+    <Router>
       <App />
-    </main>
+    </Router>
   </React.StrictMode>,
 )
