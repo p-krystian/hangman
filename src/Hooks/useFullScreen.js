6dbb7e54b366ffa6022048a6054e09791daeb663
Krystian Piątek <piatekkrystian13@gmail.com>
2024-01-26 11:02:02 +0100

    Cleanup in useFullScreen

--- src/Hooks/useFullScreen.js
+++ src/Hooks/useFullScreen.js
@@ -1,7 +1,8 @@
 function useFullScreen(){
   const root = document.getElementById('root')
-  console.log('used Full')
+
   root.classList.add('full')
+
   return () => root.classList.remove('full')
 }
 export default useFullScreen
