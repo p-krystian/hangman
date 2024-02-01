ac3c49dc0f1046f4cba1f53faaa00c5e3a7b69f2
Krystian Piątek <piatekkrystian13@gmail.com>
2024-02-01 12:22:31 +0100

    Create a makeshift version of Multi

--- src/Screens/Multi/Multi.jsx
+++ src/Screens/Multi/Multi.jsx
@@ -1,14 +1,24 @@
 import Games from '../../Views/OnlineGames/OnlineGames'
 import Create from '../../Views/CreateGame/CreateGame'
 import WriteEntry from '../../Views/WriteEntry/WriteEntry'
+import Game from '../../Views/Game/Game'
+import EndGame from '../../Views/EndGame/EndGame'
+import GameContext from '../../Contexts/GameContext'
 import { io } from "socket.io-client"
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 const socket = io('http://127.0.0.1:8090');
 
 function MultiPlayer(){
   const [stage, setStage] = useState('loading')
   const [gameList, setGameList] = useState([])
+  const gameData = useRef({
+    entry: '',
+    nicks: ['Ty', 'Przeciwnik'],
+    points: [0, 0],
+    rounds: [0, 0],
+    win: false
+  })
 
   useEffect(() => {
     socket.connect()
@@ -16,7 +26,7 @@ function MultiPlayer(){
 
     socket.on('game-list', games => {
       setGameList(games)
-      setStage('games')
+      setStage('lobby')
     })
     socket.on('wait-start', () => {
       setStage('waiting')
@@ -29,10 +39,14 @@ function MultiPlayer(){
       setStage('loading')
     });
     socket.on('start-game', phrase => {
+      gameData.current.entry = phrase
       setStage('game')
     });
     socket.on('game-data', data => {
-      console.log(data)
+      gameData.current.points[0] = data.wins
+      gameData.current.points[1] = data.oWins
+      gameData.current.rounds[0] = data.rounds
+      gameData.current.rounds[1] = data.oRounds
       setStage('result')
     })
 
@@ -40,26 +54,38 @@ function MultiPlayer(){
   }, [])
 
   return (
-    stage === 'loading' ? (
-      <h2>Ładowanie</h2>
-    ) : stage === 'games' ? (
-      <Games
-        gameList={ gameList }
-        onJoin={ id => socket.emit('join-game', id) }
-        onCreate={ () => setStage('create') }
-      />
-    ) : stage === 'create' ? (
-      <Create
-        back={ () => setStage('games') }
-        submit={ name => socket.emit('create-game', name) }
-      />
-    ) : stage === 'phrase' ? (
-      <WriteEntry
-        nick={ 'Przeciwnika' }
-      />
-    ) : (
-      <h2>Czekanie na przeciwnika</h2>
-    )
+    <GameContext.Provider value={ gameData.current }>{
+      stage === 'loading' ? (
+        <h2>Ładowanie</h2>
+      ) : stage === 'lobby' ? (
+        <Games
+          gameList={ gameList }
+          onJoin={ id => socket.emit('join-game', id) }
+          onCreate={ () => setStage('create') }
+        />
+      ) : stage === 'create' ? (
+        <Create
+          back={ () => setStage('games') }
+          submit={ name => socket.emit('create-game', name) }
+        />
+      ) : stage === 'phrase' ? (
+        <WriteEntry
+          nick={ 'PRZECIWNIKA' }
+          next={ () => socket.emit('write-phrase', gameData.current.entry) }
+        />
+      ) : stage === 'game' ? (
+        <Game
+          onWin={ () => socket.emit('end-game', gameData.current.entry) }
+          onLose={ () => socket.emit('end-game', false) }
+        />
+    ) : stage === 'result' ? (
+        <EndGame
+          next={ () => socket.emit('join-lobby') }
+        />
+      ) : (
+        <h2>Czekanie na przeciwnika</h2>
+      )
+    }</GameContext.Provider>
   )
 }
 
