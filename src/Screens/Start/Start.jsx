cb9670c0ef11a271821f255ce7cf2c7bb2c47905
Krystian Piątek <piatekkrystian13@gmail.com>
2024-02-05 14:35:36 +0100

    Active exit button in Start menu

--- src/Screens/Start/Start.jsx
+++ src/Screens/Start/Start.jsx
@@ -2,12 +2,16 @@ import Button from '../../Components/Button/Button.jsx'
 import styles from './Start.module.css'
 
 function MenuStart(){
+  function exit(){
+    window.close()
+    location.href = import.meta.env.VITE_EXIT_URL
+  }
   return (
     <div className = {styles.buttons}>
       <Button link='/single'>Solo</Button>
       <Button link='/local'>Lokalnie</Button>
       <Button link='/multi'>Online</Button>
-      <Button>Wyjdź</Button>
+      <Button onClick={ exit }>Wyjdź</Button>
     </div>
   )
 }
