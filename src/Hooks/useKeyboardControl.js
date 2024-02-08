60117bd68907375158a67932b91c63cbae488b82
Krystian Piątek <piatekkrystian13@gmail.com>
2024-02-08 10:19:25 +0100

    Add left and right key event listener in useKeyboardControl

--- src/Hooks/useKeyboardControl.js
+++ src/Hooks/useKeyboardControl.js
@@ -2,7 +2,9 @@ function useKeyboardControl(back, enter, up, down){
   const actions = {
     27: back,
     13: enter,
+    37: up,
     38: up,
+    39: down,
     40: down
   }
   const listen = e => {
