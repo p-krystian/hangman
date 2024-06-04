a33a4fcc9e100c08ab354144cc69aa1d52087e6d
Krystian Piątek <piatekkrystian13@gmail.com>
2024-06-04 18:41:41 +0200

    Create InfoSymbol component

---
+++ src/Components/InfoSymbol/InfoSymbol.jsx
@@ -0,0 +1,10 @@
+function InfoSymbol(){
+  return (
+    <svg viewBox="0 0 64 64" style={ {height: '100%'} }>
+      <circle r="28" cx="32" cy="32" fill="none" stroke="currentColor" strokeWidth="6" />
+      <circle r="5" cx="32" cy="20" fill="currentColor" />
+      <line x1="32" y1="30" x2="32" y2="50" stroke="currentColor" strokeWidth="6" />
+    </svg>
+  )
+}
+export default InfoSymbol
