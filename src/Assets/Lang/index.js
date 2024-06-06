da1672112f55cd5520f24db41c7d3a973c00ae9d
Krystian Piątek <piatekkrystian13@gmail.com>
2024-06-06 13:31:13 +0200

    Add language flags

--- src/Assets/Lang/index.js
+++ src/Assets/Lang/index.js
@@ -1,18 +1,22 @@
 import polish from './pl.json'
 import polishWords from './words-PL.json'
 import polishLogo from '../Images/logo-alpha.svg'
+import polishFlag from '../Images/flag-pl.svg'
 import english from './en.json'
+import englishFlag from '../Images/flag-en.svg'
 
 export default {
   availables: ['pl', 'en'],
   pl: {
     language: polish,
     words: polishWords,
-    logo: polishLogo
+    logo: polishLogo,
+    flag: polishFlag
   },
   en: {
     language: english,
     words: {},
-    logo: polishLogo
+    logo: polishLogo,
+    flag: englishFlag
   }
 }
