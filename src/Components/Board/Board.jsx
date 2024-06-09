bfe503831dfeefb28503a51e9030fdf24af5eff8
Krystian Piątek <piatekkrystian13@gmail.com>
2024-06-09 22:55:56 +0200

    Make Board size depend on Keyboard height

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -1,11 +1,17 @@
 import styles from './Board.module.css'
 import svgs from '../../Assets/Animation'
+import useLanguage from '../../Hooks/useLanguage'
 
 function Board({ progress, small }){
   const imagesName = svgs.slice(0, progress)
+  const [l] = useLanguage()
+  const keyboardSize = Math.ceil(l('alphabet').length / 7)
 
   return (
-    <div className={ `${styles.board} ${small ? styles.small : ''}` }>
+    <div
+      className={ `${styles.board} ${small ? styles.small : ''}` }
+      style={ {'--s': keyboardSize} }
+    >
       { imagesName.map((src, index) => (
         <img
           key={ `img-${index}` }
