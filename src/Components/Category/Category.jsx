22e7249aee996c7e0c1de27d749b66b5df45a555
Krystian Piątek <piatekkrystian13@gmail.com>
2024-06-10 17:44:03 +0200

    Make Category component working

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -2,17 +2,35 @@ import useLanguage from '../../Hooks/useLanguage'
 import { useState, useEffect } from 'react'
 import styles from './Category.module.css'
 
+const cache = {word: 'null', category: 'undefinded'}
+const findCategory = (word, dict, unknown) => {
+  for (const cat of Object.keys(dict)){
+    if (dict[cat].find(e => e.toUpperCase() === word.toUpperCase())){
+      return cat
+    }
+  }
+  return unknown
+}
+
 function Category({ entry, short }){
-  const [category, setCategory] = useState('...')
-  const [l, extra] = useLanguage()
+  const [category, setCategory] = useState('\t')
+  const [l, extraLang] = useLanguage()
   const keyboardSize = Math.ceil(l('alphabet').length / 7)
-  const words = extra().words
+  const words = extraLang().words
 
   useEffect(() => {
-    setTimeout(
-      () => setCategory(l('categoryUnknown')),
-      500
-    )
+    if (entry === cache.word){
+      setCategory(cache.category)
+      return
+    }
+    cache.word = entry
+    cache.category = findCategory(entry, words, l('categoryUnknown'))
+    for (let i = 0; i <= cache.category.length; i++){
+      setTimeout(
+        () => setCategory(cache.category.substring(0, i)),
+        i * 150
+      )
+    }
   }, [entry, words])
 
   return (
