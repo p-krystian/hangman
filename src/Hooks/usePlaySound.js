3e63b111afe95cac1ffbd6165330d6827a34f72d
Krystian Piątek <piatekkrystian13@gmail.com>
2024-06-11 18:37:47 +0200

    Don't display errors from usePlaySound hook

--- src/Hooks/usePlaySound.js
+++ src/Hooks/usePlaySound.js
@@ -3,23 +3,21 @@ import useSettings from './useSettings'
 
 for (const file of Object.values(files)){
   const audio = new Audio(file)
-  audio.volume = 10 ** -5
+  audio.load()
+  audio.volume = 10 ** -6
   audio.autoplay = true
-  try{ audio.play() }
-  catch{}
+  audio.play().catch(() => `Don't throw error`)
 }
 
 function usePlaySound(name){
   const [getSettings] = useSettings()
 
-  if (!files[name] || getSettings().soundVolume < 1)
+  if (!files[name] || getSettings().soundVolume < 1){
     return
-
+  }
   const audio = new Audio(files[name])
-  audio.preload = true
+  audio.load()
   audio.volume = getSettings().soundVolume * 0.2
-
-  try{ audio.play() }
-  catch{}
+  audio.play().catch(() => `Don't throw error`)
 }
 export default usePlaySound
