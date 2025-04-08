f6a829dee9c23f84924ae4228222d884ee1aa0fa
Krystian Piątek <piatekkrystian13@gmail.com>
2025-04-08 21:47:54 +0200

    Convert useKeyboardControl hook to TS

---
+++ src/Hooks/useKeyboardControl.ts
@@ -0,0 +1,35 @@
+function useKeyboardControl(
+  back?: () => void,
+  enter?: () => void,
+  up?: () => void,
+  down?: () => void
+){
+  const actions = {
+    'escape': back,
+    'enter': enter,
+    'arrowup': up,
+    'arrowleft': up,
+    'arrowright': down,
+    'arrowdown': down
+  } as const
+
+  const listen = (e: KeyboardEvent) => {
+    const code = e.key.toLowerCase()
+
+    if (!Object.keys(actions).includes(code)){
+      return
+    }
+
+    const key = code as keyof typeof actions
+    const action = actions[key]
+    console.log(action)
+    if (typeof action === 'function'){
+      action()
+    }
+
+  }
+  window.addEventListener('keyup', listen)
+
+  return () => window.removeEventListener('keyup', listen)
+}
+export default () => useKeyboardControl
