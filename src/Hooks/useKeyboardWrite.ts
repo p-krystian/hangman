ff5d19d2e9c76f9bbf97ab6987ce291af7c001ed
Krystian Piątek <piatekkrystian13@gmail.com>
2025-04-08 21:48:10 +0200

    Convert useKeyboardWrite hook to TS

---
+++ src/Hooks/useKeyboardWrite.ts
@@ -0,0 +1,24 @@
+function useKeyboardWrite(
+  setText:React.Dispatch<React.SetStateAction<string>>,
+  maxLength:number
+){
+  function write(current:string, char:string){
+    char = char.replace('^32', ' ')
+    if (char === '^8'){
+      return current.slice(0, -1)
+    }
+    if (
+      char == ' ' && (current[current.length - 1] == ' '
+      || current.length + 1 >= maxLength)
+    ){
+      return current
+    }
+
+    let newText = current.trimStart().toUpperCase()
+    newText += char
+
+    return newText.substring(0, maxLength)
+  }
+  return (char:string) => setText(current => write(current, char))
+}
+export default useKeyboardWrite
