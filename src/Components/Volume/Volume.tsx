2e301ab1b03f637081f0cda68849430bb13a62ba
Krystian Piątek <piatekkrystian13@gmail.com>
2025-08-18 17:18:08 +0200

    Make vlume-switch button working

--- src/Components/Volume/Volume.tsx
+++ src/Components/Volume/Volume.tsx
@@ -1,12 +1,11 @@
-import { useImperativeHandle, useState } from 'react';
-import useSettings from '@/Hooks/useSettings';
-import useLanguage from '@/Hooks/useLanguage';
+import AppContext from '@/Contexts/AppContext';
+import useLanguage from '@/Hooks/useLang';
+import { useContext, useImperativeHandle } from 'react';
 import styles from './Volume.module.css';
 
-function Volume({ ref }: { ref: React.ForwardedRef<unknown> }) {
-  const [getSettings, setSettings] = useSettings();
-  const [l] = useLanguage();
-  const [vol, setVol] = useState(getSettings().soundVolume);
+function Volume({ ref }: { ref: React.ForwardedRef<{ click: () => void }> }) {
+  const { volume, setVolume } = useContext(AppContext);
+  const { l } = useLanguage();
 
   const levels = [
     <path d="m 13,2 c 0,0 5,6 0,12" fill="none" stroke="currentColor" strokeWidth="1.2" key="v1" />,
@@ -15,25 +14,21 @@ function Volume({ ref }: { ref: React.ForwardedRef<unknown> }) {
   ];
 
   useImperativeHandle(ref, () => ({
-    click: () => setVol(currentVol => {
-      const newVol = currentVol > 2 ? 0 : currentVol + 1;
-      setSettings('soundVolume', newVol);
-      return newVol;
-    }),
-  }), [setSettings]);
+    click: () => setVolume((volume > 2 ? 0 : volume + 1) as typeof volume),
+  }), [volume, setVolume]);
 
   return (
-    <svg viewBox="0 0 16 16" className={ styles.icon }>
-      <title>{ l('volumeWord') }</title>
+    <svg viewBox="0 0 16 16" className={styles.icon}>
+      <title>{l('volumeWord')}</title>
       <path fill="currentColor" d="M 7.7,0 C 7.5,0 7.3,0 7.1,0.2 L 3.3,4.4 H 0.5 C 0.2,4.4 2.8e-5,4.6 0,4.9 v 6.9 c 2.8e-5,0.3 0.2,0.5 0.5,0.5 h 2.8 l 3.9,3.6 C 7.5,16.2 8,15.9 8,15.5 V 0.5 C 8,0.3 7.9,0.1 7.7,0 Z" />
-      { vol === 0 ? (
+      {volume === 0 ? (
         <>
           <line x1="11" y1="6" x2="15" y2="10" stroke="currentColor" strokeWidth="1.5" />
           <line x1="15" y1="6" x2="11" y2="10" stroke="currentColor" strokeWidth="1.5" />
         </>
       ) : (
-        levels.splice(vol * (-1))
-      ) }
+        levels.splice(volume * (-1))
+      )}
     </svg>
   );
 }
