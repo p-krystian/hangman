8f104e7c969269fd18555a35a2073048037a9931
Krystian Piątek <piatekkrystian13@gmail.com>
2025-09-01 12:27:39 +0200

    Move SVG symbols to .svg files

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   build: { target: 'es2020' },
-  plugins: [react()],
+  plugins: [svgr(), react()],
   server: {
     host: '0.0.0.0',
     port: 8080,
