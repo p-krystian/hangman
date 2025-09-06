bb0665253338fa58c7d965b4d9a6f7008ea844a5
Krystian Piątek <piatekkrystian13@gmail.com>
2025-09-06 14:28:56 +0200

    Add warning comment about imperative DOM usage in preload.ts

--- src/Utils/preload.ts
+++ src/Utils/preload.ts
@@ -1,3 +1,9 @@
+/**
+ * WARNING: This file adds elements to the DOM imperatively,
+ * outside of React's control.
+ * The '#preload' container is NOT part of the React component tree.
+ */
+
 const container = document.getElementById('preload')!;
 const preloaded = new Set<string>();
 
